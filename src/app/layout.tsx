cee4fdd7020052a3a56fc9bc2c6781dd51f4809c
Anmol Raj <araj97855@gmail.com>
2026-09-06 00:38:09 +0530

    feat: top scroll progress bar with smooth spring animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import ScrollLock from "@/components/ScrollLock";
 import { CommandPalette } from "@/components/CommandPalette";
+import { ScrollProgress } from "@/components/ScrollProgress";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -133,6 +134,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
         <ScrollLock />
+        <ScrollProgress />
         <Navbar />
         {children}
         <Footer />
