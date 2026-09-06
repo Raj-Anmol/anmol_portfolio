3f27f47ac39d1a241694d6a9a9bf504ddaece640
Anmol Raj <araj97855@gmail.com>
2026-09-07 00:21:45 +0530

    Remove Certificates section from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Experience } from "@/components/Experience";
 import { Projects } from "@/components/Projects";
 import { Skills } from "@/components/Skills";
 import { Education } from "@/components/Education";
-import { Certificates } from "@/components/Certificates";
 import { Contact } from "@/components/Contact";
 
 export default function Home() {
@@ -17,7 +16,6 @@ export default function Home() {
         <Projects />
         <Skills />
         <Education />
-        <Certificates />
         <Contact />
       </main>
     </div>
